daabd1252a761b471afbf9f43aa0461003c91201
ajacqm <kubamatuszko@gmail.com>
2020-10-25 20:46:48 +0100

    Wersja 0.5: - Przygotowanie podstawowego kodu ktory bedzie pozniej uzupelniony

--- main.cpp
+++ main.cpp
@@ -6,7 +6,27 @@ zachowujuc kolejnosc ich pierwszego wystapienia.*/
 
 using namespace std;
 
+int n;
+
+void sort(int tab[])
+{
+}
+
 int main()
 {
+    cout << "Wprowadz ilosc elementow: ";
+    cin >> n;
+    int tab[n];
+    for(int i=0; i<n; i++)
+    {
+        cout << "Wprowadz " << i+1 << " element tablicy: ";
+        cin >> tab[i];
+    }
+    sort(tab); //Przekazanie tablicy do funkcji
+    for(int i=0; i<n; i++)
+    {
+        cout << "Element: " << i+1 << " jest rowny: " << tab[i] << endl;
+    }
+
     return 0;
 }
