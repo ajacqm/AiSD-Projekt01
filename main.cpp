54fb6c4b65878026e6ddf4bf20e5501c8aa65804
ajacqm <kubamatuszko@gmail.com>
2020-10-25 22:03:01 +0100

    Wersja 0.6: - Dodano kod do funkcji sort(na obecna chwile dziala poprawnie)

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 /*Uporzadkuj zadane tablice wielu powtarzajacych sie elementow, tak aby zgromadzic te same elementy
 zachowujuc kolejnosc ich pierwszego wystapienia.*/
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,7 +11,30 @@ int n;
 
 void sort(int tab[])
 {
+    /*Ogolnie cel jest taki zeby przesuwac tablice w prawo*/
+    int znacznik, temp;
+    for(int i=0; i<n; i++)
+    {
+        znacznik = tab[i];
+        for(int j=i+1; j<n; j++) //1+1 ? - sprawdzic pozniej czy jest okej
+        {
+            if(tab[i]!=tab[j])
+            {
+                for(int k=j+1; k<n; k++)
+                {
+                    if(tab[k]==znacznik)
+                    {
+                        for(int p=k; p>j; p--)
+                        {
+                            swap(tab[p],tab[p-1]); //przesuwanie elementow tablicy
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
+
 
 int main()
 {
