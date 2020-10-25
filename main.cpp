f0a4dd669c8136d263ca7602b4a71eb36745937f
ajacqm <kubamatuszko@gmail.com>
2020-10-25 15:07:22 +0100

    Wersja 0.1: - Utworzenie projektu w Code:Blocks

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*Algorytmy i Struktury Danych*/
+/*Jakub Matuszko Grupa 5*/
+/*Uporz¹dkuj zadan¹ tablicê wielu powtarzaj¹cych siê elementów, tak aby zgromadziæ te same elementy
+zachowuj¹c kolejnoœæ ich pierwszego wyst¹pienia.*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
