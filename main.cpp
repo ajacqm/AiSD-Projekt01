c78ad6a641c55616e3f2ac333a16719121d0aa4a
ajacqm <kubamatuszko@gmail.com>
2020-12-03 12:18:41 +0100

    Wersja 1.3: - Zabezpieczenie przed bledem wczytania - Dodanie komentarzy

--- main.cpp
+++ main.cpp
@@ -8,52 +8,56 @@ zachowujuc kolejnosc ich pierwszego wystapienia.*/
 using namespace std;
 
 
-void f_swap(int &a,int &b)//Funkcja przestawiajaca dwa elementy
+void f_swap(int &a,int &b)  //Funkcja zmieniajaca miejscami dwie liczby (poprzez podanie adresu liczby)
 {
-    int temp;
-    temp = a;
-    a = b;
-    b = temp;
+    int temp = a;           //Zmienna temp - przechowuje tymczasowa wartosc liczby a;
+    a = b;                  //Przypisanie zmiennej a watrosci z liczby b
+    b = temp;               //Przypisanie zmiennej b wartosci tymczasowej
 }
 
-void sort(vector <int> &numbers) //Funkcja sortujaca - wzgledem kolejnosci wystepowania elementow
+void sort(vector <int> &numbers)                        //Funkcja sortujaca wzgledem wystapienia liczb
 {
-    for(int i=0; i<numbers.size(); i++)
+    for(int i=0; i<numbers.size(); i++)                 //Petla i, to do wektora z jej indeksem porownywane sa liczby
     {
-        for(int j=i+1; j<numbers.size(); j++)
+        for(int j=i+1; j<numbers.size(); j++)           //Petla j, druga liczba z wektora o indeksie j jest porownywana z i
         {
-            if(numbers[i]==numbers[j])
+            if(numbers[i]==numbers[j])                  //Porownanie liczby o indeksie i do liczby o indeksie j
             {
-                for(int k=j; k>i+1; k--)
-                {
-                    f_swap(numbers[k],numbers[k-1]);
+                for(int k=j; k>i+1; k--)                //Petla odpowiadajaca za przestawianie liczb, zamienia dwie obok siebie
+                {                                       //jej celem jest przesuniecie liczby z pozycji j do pozycji i+1
+                    f_swap(numbers[k],numbers[k-1]);    //Funkcja przestawiajaca dwa elementy
                 }
-                i++; //Inkrementacja i, przestawia pierwszy wskaznik po znalezieniu liczby rownej numbers[i];
-            }
+                i++;                                    //Po odnalezieniu liczby rownej, zostaje przesuniety dodatkowo o jedno
+            }                                           //miejsce indeks i
         }
     }
 }
 
 int main()
 {
-    ifstream input("input.txt");
-    ofstream output("output.txt");
+    ifstream input("input.txt");            //Deklaracja pliku wejsciowego
+    ofstream output("output.txt");          //Deklaracja pliku wyjsciowego
     int n;
-    vector <int> numbers;
-    if(!input.good())
+    vector <int> numbers;                   //Deklaracja wektora liczb
+    if(!input.good())                       //Sprawdzanie poprawnosci otwarcia pliku
     {
-        cerr << "File error";
-        return 1;
+        cerr << "File error";               //Przekazanie do konsoli informacji o bledzie pliku
+        return 1;                           //Zwrocenie wartosci 1, konczy program
     }
-    else
+    else                                    //W przeciwnym wypadku:
     {
-        while(!input.eof())
-        {
+        while(!input.eof())                 //Petla wczytujaca liczby do wektora do napotkania
+        {                                   //konca pliku
             input >> n;
-            numbers.push_back(n);
+            if(input.fail())                //Sprawdzenie poprawnosci wczytanej liczby
+            {                               //Jezeli wejscie wskaze blad:
+                input.clear();              //Zostanie usunieta flaga bledu z wejscia
+                input.ignore();             //Zostanie pominiety jeden znak
+            }
+            else numbers.push_back(n);      //Gdy nie bedzie bledu, program wczyta liczbe na koniec wektora
         }
-        sort(numbers);
-        for(int i=0; i<numbers.size();i++)
+        sort(numbers);                      //Przejscie do funkcji sortujacej
+        for(int i=0; i<numbers.size();i++)  //Wypisanie posortowanych liczb do pliku
         {
             output << numbers[i] << "; ";
         }
